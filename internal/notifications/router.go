b08574fed1c777b6e88542ffd8cc9c2e64d3e8ea
Matt Panton <matthew.panton@gmail.com>
2026-09-01 19:14:47 -0400

    fix: keep Pushover within installation audiences

--- internal/notifications/router.go
+++ internal/notifications/router.go
@@ -127,8 +127,6 @@ func installationDestinationMatches(means Means, topic Topic, recipient Recipien
 	switch topic {
 	case TopicProposalSubmitted:
 		return recipient == RecipientApprovers
-	case TopicProposalApproved, TopicProposalDeclined:
-		return means == MeansPushover && recipient == RecipientPerson
 	case TopicAcquisitionAvailable, TopicAcquisitionGaveUp, TopicChannelLive, TopicChannelDegraded:
 		return recipient == RecipientOperators
 	default:
