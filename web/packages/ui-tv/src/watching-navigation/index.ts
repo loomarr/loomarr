3b6e5318cb7f8cc7e55f0d401a480f5e257f39eb
Matt Panton <mantonx@users.noreply.github.com>
2026-09-04 08:19:57 +0000

    feat(clients): complete TV navigation adapters (#990)

---
+++ web/packages/ui-tv/src/watching-navigation/index.ts
@@ -0,0 +1,14 @@
+export {
+  initialTvWatchingRemoteState,
+  reduceTvWatchingRemote,
+  tvNumberEntryPresentation,
+} from "./watching-navigation";
+export type {
+  TvNumberEntryPresentation,
+  TvNumberedChannel,
+  TvRemoteDigit,
+  TvWatchingRemoteEvent,
+  TvWatchingRemoteIntent,
+  TvWatchingRemoteResult,
+  TvWatchingRemoteState,
+} from "./watching-navigation.type";
